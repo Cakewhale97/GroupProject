59adda1784fc90fa4e9ea9d10d90aee7328ff0b2
JassinAlSafe <121439769+Cakewhale97@users.noreply.github.com>
2024-03-22 12:53:45 +0100

    update

---
+++ vite-project/src/Router/Routes.jsx
@@ -0,0 +1,25 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// Import the components for each of your routes
+import LandingPage from "../pages/LandingPage";
+import Menu from "../pages/Menu";
+import Cart from "../components/Cart";
+import Nav from "../pages/Nav";
+import OrderStatus from "../pages/OrderStatus";
+
+//Define a component for your apps router.
+const AppRouter = () => {
+  return (
+    // Wrap eveything in a Router component.  This sets up the context for routing.
+    <Router>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/menu" element={<Menu />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/orderstatus" element={<OrderStatus />} />
+        <Route path="/nav" element={<Nav />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default AppRouter;

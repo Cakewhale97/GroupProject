0d9e46ed645f57102e0f2f0d72d35ee56a476939
JassinAlSafe <121439769+Cakewhale97@users.noreply.github.com>
2024-03-22 15:11:22 +0100

    ipdate

--- vite-project/src/components/Products.jsx
+++ vite-project/src/components/Products.jsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import axios from "axios";
+import { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { addToCart } from "../Redux/cartSlice";
 
-function Products() {
+export default function Products() {
+  const dispatch = useDispatch();
+  const [coffeeItems, setCoffeeItems] = useState([]);
+
+  const handleAddToCart = (item) => {
+    console.log("added to cart", item);
+    dispatch(addToCart(item));
+  };
+
+  useEffect(() => {
+    axios
+      .get("https://airbean-api-xjlcn.ondigitalocean.app/api/beans/")
+      .then((res) => {
+        setCoffeeItems(res.data.menu);
+        console.log(res.data.menu);
+      })
+      .catch((error) => {
+        console.error("There was an ", error);
+      });
+  }, []);
+
   return (
-    <div>Products</div>
-  )
+    <div className="Products">
+      {coffeeItems.map((item, index) => (
+        <div key={index}>
+          <div className="AddBtn">
+            <button onClick={() => handleAddToCart(item)}>+</button>
+          </div>
+          <div>
+            <h1>{item.title}</h1>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 }
-
-export default Products

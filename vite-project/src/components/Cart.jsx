04b4e505976249bbfa5e42981717ad6875f0b772
Robert-Tyborn <144322797+Robert-Tyborn@users.noreply.github.com>
2024-03-25 10:08:26 +0100

    cart map

--- vite-project/src/components/Cart.jsx
+++ vite-project/src/components/Cart.jsx
@@ -4,13 +4,15 @@ import { useSelector } from "react-redux";
 
 export default function Cart() {
   const cartItems = useSelector((state) => state.cart);
+  console.log('cart items', cartItems)
 
   return (
-   // {cartItems.map((item, index) => (
-    //  <div key={index}>
-    <div className="Cart">
-      <h1>cart</h1>
-      <p></p>
+    <div className="Products">
+      {cartItems.map((item, index) => (
+        <div key={index}>
+     
+        </div>
+      ))}
     </div>
   );
 }

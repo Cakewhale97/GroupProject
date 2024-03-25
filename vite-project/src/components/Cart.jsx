474cda6704154a83b1a315fca3e673c08bebcc25
JassinAlSafe <121439769+Cakewhale97@users.noreply.github.com>
2024-03-25 13:03:40 +0100

    update

--- vite-project/src/components/Cart.jsx
+++ vite-project/src/components/Cart.jsx
@@ -1,26 +1,27 @@
-import './Cart.css'
+import "./Cart.css";
 import { useSelector } from "react-redux";
 import SendOrder from "./SendOrder";
+import Nav from "../pages/Nav";
 
 export default function Cart() {
   const cartItems = useSelector((state) => state.cart);
-  console.log('cart items', cartItems)
+  console.log("cart items", cartItems);
 
   return (
     <div className="cart-overlay">
       <div className="cart">
         <div className="cart-container">
           <div className="nav-container">
-            <img className="nav" src="src/assets/navicon.svg" alt="" />   </div>
-          <div className="cart-icon">   </div>
-
+            <Nav />{" "}
+          </div>
+          <div className="cart-icon"> </div>
         </div>
-        <article className='cart-order-main'>
+        <article className="cart-order-main">
           <section className="cart-order">
-            <h1 className='cart-order__title'>Min Beställning</h1>
+            <h1 className="cart-order__title">Min Beställning</h1>
             <div className="cart-order__products">
               {cartItems.map((item, index) => (
-                <div className='cart-order__product-info' key={index}>
+                <div className="cart-order__product-info" key={index}>
                   <p className="product-title">{item.title}</p>
                   <p className="cart-product-price">{item.price}kr</p>
                 </div>
@@ -28,17 +29,16 @@ export default function Cart() {
             </div>
           </section>
           <section className="cart-total">
-
-            <p className='cart-total__title'>Totalt:</p>
+            <p className="cart-total__title">Totalt:</p>
             <div className="dotted-line"></div>
-            <p className='cart-total__sum'>{cartItems.reduce((total, item) => total + item.price, 0)}kr</p>
-
+            <p className="cart-total__sum">
+              {cartItems.reduce((total, item) => total + item.price, 0)}kr
+            </p>
           </section>
-          <p className='cart-total__text'>inkl moms + drönarleverans</p>
+          <p className="cart-total__text">inkl moms + drönarleverans</p>
           <SendOrder />
         </article>
       </div>
     </div>
-
   );
 }

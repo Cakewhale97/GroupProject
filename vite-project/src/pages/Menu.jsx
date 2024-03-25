4d87a212d00ccf255eaa864b218eab1f8acaf1be
JassinAlSafe <121439769+Cakewhale97@users.noreply.github.com>
2024-03-25 13:00:28 +0100

    update

--- vite-project/src/pages/Menu.jsx
+++ vite-project/src/pages/Menu.jsx
@@ -3,42 +3,19 @@ import Products from "../components/Products";
 import { Link } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { useState } from "react";
+import Nav from "./Nav";
 
 export default function Menu() {
   const cartItems = useSelector((state) => state.cart);
-  const [isNavOpen, setIsNavOpen] = useState(false);
 
   return (
     <div className="menu">
       <div className="menu-cart-container">
-        <div className="menu-nav-container">
-          <img
-            className="nav"
-            src="src/assets/navicon.svg"
-            alt=""
-            onClick={() => setIsNavOpen(!isNavOpen)}
-          />
-          {isNavOpen && (
-            <div className="nav-menu">
-              <div className="close" onClick={() => setIsNavOpen(false)}>X</div>
-              <Link to="/menu" onClick={() => setIsNavOpen(false)}>
-                Meny
-              </Link>
-              <Link to="/about" onClick={() => setIsNavOpen(false)}>
-                Vårt Kaffe
-              </Link>
-              <Link to="/orderstatus" onClick={() => setIsNavOpen(false)}>
-                Orderstatus
-              </Link>
-            </div>
-          )}
-        </div>
+        <Nav />
         <div className="menu-cart">
-          {!isNavOpen && (
-            <Link to="/cart">
-              <p className="menu-cart-length">{cartItems.length}</p>
-            </Link>
-          )}
+          <Link to="/cart">
+            <p className="menu-cart-length">{cartItems.length}</p>
+          </Link>
         </div>
       </div>
       <h1 className="menu-title">Meny</h1>

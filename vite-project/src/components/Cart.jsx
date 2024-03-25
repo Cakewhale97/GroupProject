4417aa1b0d5c66320aaa8652b50b2d61be023b92
ElinCharlotta <janssonelin@hotmail.com>
2024-03-25 10:36:48 +0100

    Update cart
    html

--- vite-project/src/components/Cart.jsx
+++ vite-project/src/components/Cart.jsx
@@ -1,22 +1,18 @@
 import './Cart.css'
+import { useSelector } from "react-redux";
 
 
-function Cart() {
-  return (<>
-    <div className="cart-overlay">
-      <div className="cart">
-        <div className="cart-container">
-        <div className="nav-container">
-            <img className="nav" src="src/assets/navicon.svg" alt="" />
-          <div className="cart-icon">   </div>
-          </div>
+export default function Cart() {
+  const cartItems = useSelector((state) => state.cart);
+  console.log('cart items', cartItems)
 
-  
-      </div>
-      <section className="cart-order">
-        <h1>Min Beställning</h1>
-      </section>
-    </div >
+  return (
+    <div className="Products">
+      {cartItems.map((item, index) => (
+        <div key={index}>
+      
+        </div>
+      ))}
     </div>
   </>
 

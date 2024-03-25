b489dccceeda1e43f47a940c4b68a8eeeed031f9
ElinCharlotta <janssonelin@hotmail.com>
2024-03-25 10:17:45 +0100

    added code

--- vite-project/src/components/Cart.jsx
+++ vite-project/src/components/Cart.jsx
@@ -1,10 +1,25 @@
 import './Cart.css'
 
-export default function Cart() {
-  return (
-    <div className="Cart">
-      <h1>cart</h1>
-      <p></p>
+
+function Cart() {
+  return (<>
+    <div className="cart-overlay">
+      <div className="cart">
+        <div className="cart-container">
+        <div className="nav-container">
+            <img className="nav" src="src/assets/navicon.svg" alt="" />
+          <div className="cart-icon">   </div>
+          </div>
+
+  
+      </div>
+      <section className="cart-order">
+        <h1>Min Beställning</h1>
+      </section>
+    </div >
     </div>
+  </>
+
   );
 }
+export default Cart

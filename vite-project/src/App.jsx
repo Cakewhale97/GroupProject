fecec65cca4d89b89e8c56ebaeaf8c16dca997d1
ElinCharlotta <janssonelin@hotmail.com>
2024-03-25 11:00:35 +0100

    added code
    changes className in menu and added....

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,24 +1,14 @@
 import AppRouter from "./Router/Routes";
 import './App.css'
 
-<<<<<<< Updated upstream
-=======
-function App() {
->>>>>>> Stashed changes
+
 
 function App() {
   return (
-<<<<<<< Updated upstream
     <div className="App">
       <AppRouter />
     </div>
   );
-=======
-    <>
-
-    </>
-  )
->>>>>>> Stashed changes
 }
 
 export default App
--- vite-project/src/components/Cart.jsx
+++ vite-project/src/components/Cart.jsx
@@ -7,41 +7,26 @@ export default function Cart() {
   console.log('cart items', cartItems)
 
   return (
-    <div className="Products">
-      {cartItems.map((item, index) => (
-        <div key={index}>
-      
-        </div>
-      ))}
-
-  return (
-          <>
-    <div className="Products">
-      {cartItems.map((item, index) => (
-        <div key={index}>
-  
-
     <div className="cart-overlay">
       <div className="cart">
         <div className="cart-container">
-        <div className="nav-container">
+          <div className="nav-container">
             <img className="nav" src="src/assets/navicon.svg" alt="" />
-          <div className="cart-icon">   </div>
+            <div className="cart-icon">   </div>
           </div>
-
-  
-      </div>
-      <section className="cart-order">
-        <h1>Min Beställning</h1>
-
-      </section>
-    </div >
         </div>
-  
-      ))}
-
+        <section className="cart-order">
+          <h1>Min Beställning</h1>
+          <div className="Products">
+            {cartItems.map((item, index) => (
+              <div key={index}>
+                <p>{item.title}</p>
+                <p>{item.price}</p>
+              </div>
+            ))}
+          </div>
+        </section>
+      </div>
     </div>
-  </>
-
   );
 }

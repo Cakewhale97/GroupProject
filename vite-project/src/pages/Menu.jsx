b4367facc5e4c34838fa43f89607a2f38ca4342e
JassinAlSafe <121439769+Cakewhale97@users.noreply.github.com>
2024-03-25 09:34:54 +0100

    update

--- vite-project/src/pages/Menu.jsx
+++ vite-project/src/pages/Menu.jsx
@@ -1,18 +1,21 @@
-import React from "react";
 import "../styles/Menu.css";
 import Products from "../components/Products";
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 
-export default function Menu() {
+export default function Menu({ coffeeItems }) {
+  const cartItems = useSelector((state) => state.cart);
+
   return (
     <div className="menu">
       <div className="cart-container">
         <div className="nav-container">
-        <img className="nav" src="src/assets/navicon.svg" alt="" />
+          <img className="nav" src="src/assets/navicon.svg" alt="" />
         </div>
-        <Link to="/cart">
-         <div className="cart"></div>
-        </Link>
+        <div className="cart">
+          <p className="cart-length">{cartItems.length}</p>
+          <Link to="/cart"></Link>
+        </div>
       </div>
       <h1 className="menu-title">Meny</h1>
       <div>

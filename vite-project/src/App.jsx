9a1c2a78af28e3878e35530466d86e6986f39938
ElinCharlotta <janssonelin@hotmail.com>
2024-03-25 10:40:04 +0100

    update

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,13 +1,24 @@
 import AppRouter from "./Router/Routes";
 import './App.css'
 
+<<<<<<< Updated upstream
+=======
+function App() {
+>>>>>>> Stashed changes
 
 function App() {
   return (
+<<<<<<< Updated upstream
     <div className="App">
       <AppRouter />
     </div>
   );
+=======
+    <>
+
+    </>
+  )
+>>>>>>> Stashed changes
 }
 
 export default App

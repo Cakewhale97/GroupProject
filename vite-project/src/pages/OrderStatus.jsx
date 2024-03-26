6c29675b12edbb8063867b8e8969b5c6ebff3aa0
Robert-Tyborn <144322797+Robert-Tyborn@users.noreply.github.com>
2024-03-26 11:27:43 +0100

    OrderStatus getStatus

--- vite-project/src/pages/OrderStatus.jsx
+++ vite-project/src/pages/OrderStatus.jsx
@@ -1,10 +1,25 @@
 import "../styles/OrderStatus.css";
 import { useLocation } from "react-router-dom";
+import axios from "axios";
+import { useState, useEffect } from "react";
 
 function OrderStatus() {
   const location = useLocation();
   const orderNr = location.state?.orderNr;
   const eta = location.state?.eta;
+  const [orderStatus, setOrderStatus] = useState(null);
+
+  useEffect(() => {
+    if (orderNr) {
+      axios.get(`https://airbean-9pcyw.ondigitalocean.app/api/beans/order/status/${orderNr}`)
+        .then(response => {
+          setOrderStatus(response.data);
+        })
+        .catch(error => {
+          console.error('Error fetching order status:', error);
+        });
+    }
+  }, [orderNr]);
 
   return (
     <>

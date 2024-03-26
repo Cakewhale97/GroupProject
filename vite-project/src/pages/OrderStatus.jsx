65b250fd60219b873be4b9a39a4b6174de527f02
JassinAlSafe <121439769+Cakewhale97@users.noreply.github.com>
2024-03-26 11:31:30 +0100

    drone

--- vite-project/src/pages/OrderStatus.jsx
+++ vite-project/src/pages/OrderStatus.jsx
@@ -2,37 +2,41 @@ import "../styles/OrderStatus.css";
 import { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 import axios from "axios";
+import droneImage from "../assets/drone.svg";
+
 
 const OrderStatus = () => {
   const location = useLocation();
   const { orderNr } = location.state;
-  console.log("OrderNr:", orderNr)
+  console.log("OrderNr:", orderNr);
   const [eta, setEta] = useState(null);
 
   useEffect(() => {
-    axios.get(`https://airbean-9pcyw.ondigitalocean.app/api/beans/order/status/${orderNr}`)
-    .then((response) => {
-      setEta(response.data.eta);
-      console.log("Response data:", response.data);
-    }) 
-    .catch((error) => {
-      console.error("Error:", error);
-    });
+    axios
+      .get(
+        `https://airbean-9pcyw.ondigitalocean.app/api/beans/order/status/${orderNr}`
+      )
+      .then((response) => {
+        setEta(response.data.eta);
+        console.log("Response data:", response.data);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
   }, [orderNr]);
 
-
   return (
-    <div>
-      <h1>Order Status</h1>
+    <div className="status">
       <p>Your Order number is {orderNr}</p>
+      <img src={droneImage} alt="" className="drone"/>
+
       {eta ? (
         <p> Your order will be ready in {eta} minutes </p>
       ) : (
         <p>Fetching order status...</p>
       )}
-      <img src="" alt="" />
     </div>
   );
-}
+};
 
 export default OrderStatus;
